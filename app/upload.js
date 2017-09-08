c65c9b9188085184918ce67e2a4c56d52fbc4e8b
qiupenghua <947946592@163.com>
2017-09-08 11:49:25 +0800

    上传插件完善

--- app/upload.js
+++ app/upload.js
@@ -12,25 +12,37 @@
                 url: 'http://localhost:8080/upload',
                 autoProcessQueue: false,//自动上传
                 maxFiles: 10,
+                addRemoveLinks: true,//显示删除按钮
                 maxFilesize: 512,
                 //maxFilesize: 100,
                 //paramName: "uploadfile",
                 //maxThumbnailFilesize: 10,
-                //
-                //parallelUploads: 2,
-
-                dictResponseError: '服务器未响应：{{statusCode}}',
-                //uploadMultiple:true,
                 //parallelUploads:3,
                 //maxFiles:3,
                 //acceptedFiles:'.png,.txt'
+                //parallelUploads: 2,
+
+                dictResponseError: '服务器未响应：{{statusCode}}',
+                dictRemoveFile: '删除',
+                dictDefaultMessage: "Drop files here to upload",
+                dictFallbackMessage: "浏览器不支持.",
+                dictFallbackText: "Please use the fallback form below to upload your files like in the olden days.",
+                dictFileTooBig: "文件太大 ({{filesize}}MiB). 最大值: {{maxFilesize}}MiB.",
+                dictInvalidFileType: "你不能上传此类型文件.",
+                dictCancelUpload: "取消上传",
+                dictCancelUploadConfirmation: "确定取消上传?",
+                dictRemoveFileConfirmation: null,
+                dictMaxFilesExceeded: "上传文件数量超出最大值.",
+                uploadMultiple: true,
+
             };
 
             var eventHandlers = {
                 'addedfile': function (file) {
+                    console.log(file);
                     scope.file = file;
                     if (this.files[1] != null) {
-                        this.removeFile(this.files[0]);
+                        //this.removeFile(this.files[0]);
                     }
                     scope.$apply(function () {
                         scope.fileAdded = true;
--- demo/controller/DemoController.js
+++ demo/controller/DemoController.js
@@ -58,6 +58,7 @@
         $scope.partialDownloadLink = 'http://localhost:8080/download?filename=';
         $scope.filename = '';
 
+        $scope.fileType = typeof $scope.file;
         $scope.uploadFile = function () {
             $scope.processDropzone();
         };
